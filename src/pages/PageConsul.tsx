29967fa36c8a4d3fb1e694425bb547a502eab7b1
Eric Reeves <eric@alluvium.com>
2022-09-17 11:55:36 -0500

    Update

--- src/pages/PageConsul.tsx
+++ src/pages/PageConsul.tsx
@@ -1,98 +1,163 @@
 import React from 'react'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
-import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import { Nav } from '../components/Layout'
 import {
-    createOrderedMap, createStore, JsonSchema,
-    onChangeHandler, StoreSchemaType, storeUpdater,
-    UIStoreProvider, StoreKeys, ObjectGroup,
-    PluginStack, applyPluginStack,
+    createOrderedMap, createStore, injectPluginStack, JsonSchema,
+    onChangeHandler, storeUpdater,
+    UIStoreProvider,
 } from '@ui-schema/ui-schema'
-import { List, OrderedMap } from 'immutable'
-import { StringRenderer } from '@ui-schema/ds-material/Widgets/TextField'
-import { WidgetCountrySelect } from '../components/Widgets/WidgetCountrySelect'
+import { OrderedMap } from 'immutable'
+import { GridContainer } from '@ui-schema/ds-material/GridContainer'
 import { DataDebug } from '../components/DataDebug'
 
 const schema = createOrderedMap({
-    type: 'object',
-    properties: {
-        name: {
-            type: 'string',
+    "type": "object",
+    "title": "Consul Agent Configuration",
+    "required": ["node_name", "data_dir", "log_level"],
+
+    "definitions": {
+        "ip": {
+            "type": "string",
+            "title": "IP Address (ex: 0.0.0.0)"
         },
-        favorites: {
-            type: 'array',
-            widget: 'SelectChips',
-            items: {
-                oneOf: [
-                    {
-                        const: 'Chips',
-                    }, {
-                        const: 'Crisps',
-                    }, {
-                        const: 'Tacos',
-                    }, {
-                        const: 'Nachos',
-                    }, {
-                        const: 'Cheese',
-                    },
-                ],
+        "directory": {
+            "type": "string",
+            "title": "Filsystem Path"
+        },
+        "log_level": {
+            "type": "string",
+            "widget": "Select",
+            "enum": ["INFO", "WARN", "DEBUG"],
+            "title": "Log Level",
+            "info": ["Log Verbosity Level"]
+        },
+        "bootstrap_expect": {
+            "type": "number",
+            "widget": "NumberSlider",
+            "exclusiveMinimum": 0,
+            "exclusiveMaximum": 8,
+            "enum": [1, 3, 5, 6, 7],
+            "view": {
+                "marks": true,
+                "marksLabel": " Nodes",
+                "tooltip": "auto",
+                "sizeXs": 10,
+                "sizeMd": 8
             },
+            "title": "Bootstrap Expect",
+            "info": [
+                "Total number of Consul servers that must peer in order for the cluster to bootstrap"
+            ]
         },
-        country: {
-            type: 'string',
-            widget: 'CountrySelect',
+        "list": {
+            "type": "array",
+            "widget": "GenericList",
+            "items": {
+                "$ref": "#/definitions/list_item"
+            }
         },
+        "list_item": {
+            "type": "string"
+        }
     },
+
+    "properties": {
+        "node_name": {
+            "type": "string",
+            "title": "Node Name",
+            "info": ["Human-Readable Node Name"]
+        },
+        "node_meta": {
+            "$ref": "#/definitions/meta_list"
+        },
+        "data_dir": {
+            "$ref": "#/definitions/directory",
+            "title": "Data Directory",
+            "info": [
+                "Local Filesystem Directory Used by Consul to Persist Data"
+            ]
+        },
+        "advertise_addr": {
+            "type": "string",
+            "title": "Advertise Address",
+            "info": [
+                "IP Address / Hostname to advertise to other Consul cluster peers (use if Consul is behind a load balancer or proxy)"
+            ]
+        },
+        "client_addr": {
+            "type": "string",
+            "title": "Client Address",
+            "info": ["Client address or something"]
+        },
+        "log_level": {
+            "$ref": "#/definitions/log_level"
+        },
+        "ui": {
+            "type": "boolean",
+            "info": "Enable the Consul Web UI?",
+            "deleteOnEmpty": true
+        },
+        "server": {
+            "type": "boolean",
+            "deleteOnEmpty": true,
+            "info": ["Is this a Consul server?  If not, it's a client."]
+        },
+        "addresses": {
+            "type": "object",
+            "widget": "Accordions",
+            "properties": {
+                "http": {
+                    "title": "Configure HTTP Listen Address(es)?",
+                    "$ref": "#/definitions/ip"
+                },
+                "https": {
+                    "title": "Configure HTTPS Listen Address(es)?",
+                    "$ref": "#/definitions/ip"
+                },
+                "dns": {
+                    "title": "Configure DNS Listen Address(es)?",
+                    "$ref": "#/definitions/ip"
+                },
+                "grpc": {
+                    "title": "Configure GRPC Listen Address(es)?",
+                    "$ref": "#/definitions/ip"
+                }
+            }
+        },
+        "verify_incoming": {
+            "type": "boolean"
+          },
+          "verify_outgoing": {
+            "type": "boolean"
+          },
+          "verify_server_hostname": {
+            "type": "boolean"
+          }
+    },
+    "dependencies": {
+        "server": {
+            "properties": {
+                "bootstrap_expect": {
+                    "$ref": "#/definitions/bootstrap_expect"
+                },
+                "datacenter": {
+                    "type": "string",
+                    "title": "Datacenter",
+                    "info": ["Datacenter ID for this Consul Instance"]
+                },
+                "primary_datacenter": {
+                    "type": "string",
+                    "title": "Primary Datacenter",
+                    "info": ["Primary Datacenter of the Cluster"]
+                }
+            }
+        }
+    }
 } as JsonSchema)
 
-const WidgetTextField = applyPluginStack(StringRenderer)
-const CountrySelect = applyPluginStack(WidgetCountrySelect)
-
-const CustomFormContent: React.FC<{
-    storeKeys?: StoreKeys
-    schema: StoreSchemaType
-    showValidity?: boolean
-}> = ({storeKeys = List(), schema, showValidity}) => {
-    const [objectSchema, setObjectSchema] = React.useState<StoreSchemaType>(() => schema)
-    return <ObjectGroup
-        storeKeys={storeKeys}
-        schema={schema} parentSchema={undefined}
-        onSchema={setObjectSchema}
-    >
-        <Grid container dir={'columns'} spacing={4}>
-            <WidgetTextField
-                level={1}
-                storeKeys={storeKeys.push('name') as StoreKeys}
-                schema={objectSchema.getIn(['properties', 'name']) as unknown as StoreSchemaType}
-                parentSchema={objectSchema}
-
-                // using `applyPluginStack`, this free-form widget is fully typed
-                // with the actual props of the widget component
-                multiline={false}
-            />
-
-            <PluginStack<{ readOnly: boolean }>
-                showValidity={showValidity}
-                storeKeys={storeKeys.push('favorites') as StoreKeys}
-                schema={objectSchema.getIn(['properties', 'favorites']) as unknown as StoreSchemaType}
-                parentSchema={objectSchema}
-                level={1}
-                readOnly={false}
-                // noGrid={false} (as grid-item is included in `PluginStack`)
-            />
-
-            <CountrySelect
-                level={1}
-                storeKeys={storeKeys.push('country') as StoreKeys}
-                schema={objectSchema.getIn(['properties', 'country']) as unknown as StoreSchemaType}
-                parentSchema={objectSchema}
-            />
-        </Grid>
-    </ObjectGroup>
-}
-
+const GridStack = injectPluginStack(GridContainer)
 const DemoComponent = () => {
     const showValidity = true
     const [store, setStore] = React.useState(() => createStore(OrderedMap({})))
@@ -108,9 +173,10 @@ const DemoComponent = () => {
             onChange={onChange}
             showValidity={showValidity}
         >
-            <CustomFormContent
+            <GridStack
                 schema={schema}
                 showValidity={showValidity}
+                isRoot
             />
 
             <DataDebug/>
